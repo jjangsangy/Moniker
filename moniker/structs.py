fd3894ef0692d9e1626b9c0fa28c3960f857d275
Sang Han <jjangsangy@gmail.com>
2014-12-27 01:32:36 -0800

    foiuwe

--- moniker/structs.py
+++ moniker/structs.py
@@ -4,5 +4,7 @@ Custom data structures.
 
 import collections
 
+__all__ = 'Tree', 'Pattern'
+
 Tree    = lambda: collections.defaultdict(Tree)
 Pattern = collections.namedtuple('Pattern', ['lookup', 'replace'])
