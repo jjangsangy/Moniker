3af08c5f95d66816188ecd93d9f67c98cd6c07bc
Sang Han <jjangsangy@gmail.com>
2014-12-27 01:38:58 -0800

    blehg

--- moniker/__init__.py
+++ moniker/__init__.py
@@ -1,7 +1,8 @@
-from .__version__ import __version__, __build__
-
 from .moniker import *
 from .structs import *
+
+from .__version__ import __version__
+from .__version__ import __build__
 
 __author__    = 'Sang Han'
 __license__   = 'Apache Software License Version 2.0'
