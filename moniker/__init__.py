0fa343118ee2ff21f755fb60e9d2470048e26b36
Sang Han <jjangsangy@gmail.com>
2014-12-27 01:36:27 -0800

    Update __init__.py

--- moniker/__init__.py
+++ moniker/__init__.py
@@ -1,6 +1,7 @@
 from .__version__ import __version__, __build__
 
 from .moniker import *
+from .structs import *
 
 __author__    = 'Sang Han'
 __license__   = 'Apache Software License Version 2.0'
